06d3f1196bbc0a545725d671d38b27bd4a178f8f
amontalvof <amontalvof92@gmail.com>
2021-03-23 14:21:16 -0600

    feat: logic for managing tasks to be done

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('colors');
 const { inquirerMenu, pause } = require('./helpers/inquirer');
+const Tasks = require('./models/tasks');
 
 const main = async () => {
     let option = '';
     do {
-        opt = await inquirerMenu();
-        console.log('\n');
+        // opt = await inquirerMenu();
+        // console.log('\n');
+        const tasks = new Tasks();
+        console.log(tasks);
         await pause();
     } while (opt !== '0');
 };
