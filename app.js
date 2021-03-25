8f1885e444d3562d7a4f17059d2b98080f3effe2
amontalvof <amontalvof92@gmail.com>
2021-03-24 22:31:33 -0600

    feat: create and list tasks

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 require('colors');
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
 const Tasks = require('./models/tasks');
 
 const main = async () => {
-    let option = '';
+    let opt = '';
+    const tasks = new Tasks();
     do {
-        // opt = await inquirerMenu();
-        // console.log('\n');
-        const tasks = new Tasks();
-        console.log(tasks);
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                const desc = await readInput('Description:');
+                tasks.createTask(desc);
+                break;
+            case '2':
+                console.log(tasks._list);
+                break;
+        }
         await pause();
     } while (opt !== '0');
 };
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -57,7 +57,26 @@ const pause = async () => {
             message: `Press ${'ENTER'.green} to continue`,
         },
     ];
+    console.log('\n');
     await inquirer.prompt(question);
 };
 
-module.exports = { inquirerMenu, pause };
+const readInput = async (message) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Please enter a value.';
+                }
+                return true;
+            },
+        },
+    ];
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+};
+
+module.exports = { inquirerMenu, pause, readInput };
