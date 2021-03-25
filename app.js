a0e9809cdaa4f5546a747c662a69de84a3ff80c5
amontalvof <amontalvof92@gmail.com>
2021-03-25 09:00:41 -0600

    feat: transform object to array

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const main = async () => {
     let opt = '';
     const tasks = new Tasks();
     do {
+        // print the menu
         opt = await inquirerMenu();
         switch (opt) {
             case '1':
+                // create option
                 const desc = await readInput('Description:');
                 tasks.createTask(desc);
                 break;
             case '2':
-                console.log(tasks._list);
+                console.log(tasks.listArray);
                 break;
         }
         await pause();
--- models/tasks.js
+++ models/tasks.js
@@ -11,6 +11,15 @@ class Tasks {
         const task = new Task(desc);
         this._list[task.id] = task;
     }
+
+    get listArray() {
+        const list = [];
+        Object.keys(this._list).forEach((key) => {
+            const task = this._list[key];
+            list.push(task);
+        });
+        return list;
+    }
 }
 
 module.exports = Tasks;
